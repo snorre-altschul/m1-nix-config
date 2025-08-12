6839488faa8f46cf76b34be723bcd32081e3e8ca
spoody <m1nix@mail.spoodythe.one>
2025-08-12 20:58:26 +0200

    guh

--- configuration.nix
+++ configuration.nix
@@ -181,7 +181,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri-session";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session";
         user = "nixos";
       };
 
